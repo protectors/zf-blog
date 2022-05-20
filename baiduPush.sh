7ac17987220750ae665c46a13dc879bd9d87e4c0
protectors <1176136493@qq.com>
2022-05-20 18:30:56 +0800

    fix: 更新百度推送

--- baiduPush.sh
+++ baiduPush.sh
@@ -6,6 +6,6 @@ set -e
 # 百度链接推送
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://www.zfprotectors.com&token=uFACJHg2uJtwAxY0"
 
-cat urls.txt
+# cat urls.txt
 
 rm -rf urls.txt # 删除文件
